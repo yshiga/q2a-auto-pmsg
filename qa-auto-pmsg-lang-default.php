c9c80a9d10bd42a317b4a89183a5f04a36d112b1
yshiga <yuichi.shiga@gmail.com>
2017-07-25 00:02:06 +0900

    翻訳ファイルのカンマ忘れのbug fix

--- qa-auto-pmsg-lang-default.php
+++ qa-auto-pmsg-lang-default.php
@@ -2,7 +2,7 @@
 return array(
 	'from_handle' => '送信者名（ハンドル）',
 	'to_posted_user' => '投稿しているユーザー向け',
-	'to_no_posted_user' => 'まだ投稿していないユーザー向け'
+	'to_no_posted_user' => 'まだ投稿していないユーザー向け',
 	'default_handle' => '管理人',
 	'default_for_posted' => '投稿しているユーザー向けメッセージ',
 	'default_for_no_post' => '投稿していないユーザー向けメッセージ',
